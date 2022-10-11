aad816d3f31fe1283c08c36bccb01ce26587488a
Ugo <upelissier@gmail.com>
2022-10-11 23:25:11 +0200

    Display & Common update
    Integration of the Agent class in common with the display part.
    
    The entire code talks with Agent now.

--- src/common.cpp
+++ src/common.cpp
@@ -29,123 +29,63 @@ bool outsideWindow(vec2 center, float ratio) {
     return false;
 }
 
-std::tuple<std::vector<vec2>, std::vector<double>, std::vector<double>> initialiazeAgents() {
+std::vector<Agent> initialiazeAgents() {
 
-    std::vector<vec2> points;
-    vec2 randomPoint;
+    std::vector<Agent> agents;
 
-    std::vector<double> angles;
+    int randomX;
+    int randomY;
     double randomAngle;
-
-    std::vector<double> velocities;
     double randomVelocity;
 
-    float ratio = (float)WIDTH / (float)HEIGHT;
-
     std::uniform_real_distribution<double> unif(0, 1); // Uniform distribution on [0:1] => Random number between 0 and 1
+    std::uniform_int_distribution uniX(0, WIDTH);
+    std::uniform_int_distribution uniY(0, HEIGHT);
     std::mt19937_64 rng;
 
     for (size_t i = 0; i<DEFAULT_NUM_AGENTS; ++i) {
 
-        randomPoint = {(float)(ratio*((2*unif(rng))-1)), (float)((2*unif(rng))-1)};
+        randomX = uniX(rng);
+        randomY = uniY(rng);
         randomAngle = 2*PI*unif(rng);
         randomVelocity = unif(rng);
 
-        if ( (not overlap(randomPoint, points)) and (not outsideWindow(randomPoint, ratio)) ) {
-            points.push_back(randomPoint);
-            angles.push_back(randomAngle);
-            velocities.push_back(randomVelocity);
-        }
+        agents.push_back(Agent(randomX,randomY,randomAngle, randomVelocity));
     }
 
-    return std::make_tuple(points, angles, velocities);
+    return agents;
 }
+
+void updateAgents(std::vector<Agent>& agents) {
+    for (Agent& agent : agents) {
+        agent.update();
+    }
+}
+
 //====================USEFUL FUNCTIONS END============================
 
-//===============Vec class functions definitions================
-
-Vec::Vec() {
-    this->x = 0;
-    this->y = 0;
-    this->angle = 0;
-    this->velocity = 0;
-}
-
-Vec::Vec(int x, int y, double angle, double velocity) {
-    this->x = x;
-    this->y = y;
-    this->angle = angle;
-    this->velocity = velocity;
-}
-int& Vec::X() {
-    return x;
-}
-
-int& Vec::Y() {
-    return y;
-}
-
-double& Vec::theta() {
-    return angle;
-}
-
-double& Vec::speed() {
-    return velocity;
-}
-
-/*Vec::operator+(Vec b) {
-    Vec result = Vec(this->X() + b.X(),
-                     this->Y() + b.Y(),
-                     this->theta() + b.theta(),
-                     this->speed() + b.speed());
-
-
-    return result;
-}
-
-Vec::operator-(Vec b) {
-    Vec result = Vec(this->X() - b.X(),
-                     this->Y() - b.Y(),
-                     this->theta() - b.theta(),
-                     this->speed() - b.speed());
-
-    return result;
-}
-
-Vec::operator*(Vec b) {
-    Vec result = Vec(this->X() * b.X(),
-                     this->Y() * b.Y(),
-                     this->theta() * b.theta(),
-                     this->speed() * b.speed());
-
-    return result;
-}
-
-Vec::Vec operator*(double b) {
-    Vec result = Vec(this->X() * b,
-                     this->Y() * b,
-                     this->theta() * b,
-                     this->speed() * b);
-
-    return result;
-}*/
-//===============Vec class functions definitions end================
-
-
 //===============Agent class functions definitions================
-
 Agent::Agent(int x, int y, double angle, double velocity) {
-
-    this->vec = Vec(x, y, angle, velocity);
+    m_x = x;
+    m_y = y;
+    m_velocity = velocity;
+    m_angle = angle;
 }
 
-/*bool Agent::update_vector() {
+int& Agent::getX() {
+    return m_x;
+}
 
-    this->vec.X() = x;
-    this->vec.Y() = y;
-    this->vec.theta() = angle;
-    this->vec.speed() = velocity;
-}*/
+int& Agent::getY() {
+    return m_y;
+}
 
+double& Agent::getAngle() {
+    return m_angle;
+}
 
-//===============Agent class functions definitions end================
+void Agent::update() {
+    m_x += 1;
+    m_y += 1;
+    m_angle += 1e-1;
+}
