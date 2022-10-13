524236f6d4f4a4b9c2140a924bfb62141cedaa1d
Ugo <upelissier@gmail.com>
2022-10-13 17:55:45 +0200

    Folders & files re-organization
    1- Deleted the useless files from /display to keep only the useful files.
    
    2- Integrated the functions of laws.hpp and laws.cpp inside the common.hpp and common.cpp files. It makes more sense for theses functions to be part of the agent class, since they rule the movement of agents.
    
    The only adaptation was to keep in mind the index of one agent when calculating its next position, so that you do not make it interact with itself
    
    3- Renamed common.hpp and common.cpp int agent.hpp and agent.cpp for more clarity and put them into the movement folder.
    
    4- Modifying all the CMake files accordingly.
    
    5- Put x and y as double. It did not change anything regarding the code. And it makes more physical sense and enable cleaner movement since we get read of "round()".

--- src/display/shaders/points.cpp
+++
@@ -1,5 +0,0 @@
-#include "points.hpp"
-
-mat3x3 points::vertex_transform_2d(float width, float height) {
-	return mat3x3{ {{{2.f / width, 0.f, 0.f}}, {{0.f, -2.f / height, 0.f}}, {{-1.f, 1.f, 1.f}}} };
-}
