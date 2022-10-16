6c760ca368287f1c9abb2eb147884aad8054332c
Ugo <upelissier@gmail.com>
2022-10-16 18:54:58 +0200

    Correction

--- test/unit_gtest.cpp
+++ test/unit_gtest.cpp
@@ -20,7 +20,7 @@ TEST(FirstGTest, ExpectCanary) {
 
 TEST(Initialization, VectorSize) {
 
-    GLFWwindow* window;
+    /* GLFWwindow* window;
     VertexArray triangle_vertexArray = {}, triangleObs_vertexArray = {};
     Buffer triangle_buffer = {0};
     ShaderProgram triangle_shaderProgram = {0};
@@ -34,17 +34,17 @@ TEST(Initialization, VectorSize) {
     std::vector<std::array<triangle::Vertex, 3>> trianglesObs;
     std::tie(agents, obstacles,triangles, trianglesObs) = initAgentWindow();
 
-    EXPECT_EQ(1, 1) << "Canary could not fail!";
-
-    /* EXPECT_EQ(agents.size(), DEFAULT_NUM_AGENTS+DEFAULT_NUM_PREDATORS) << "Not the correct number of agents!";  // with an additional message
+    EXPECT_EQ(agents.size(), DEFAULT_NUM_AGENTS+DEFAULT_NUM_PREDATORS) << "Not the correct number of agents!";  // with an additional message
     EXPECT_EQ(triangles.size(), DEFAULT_NUM_AGENTS+DEFAULT_NUM_PREDATORS) << "Not the correct number of agents!";  // with an additional message
     EXPECT_EQ(obstacles.size(), DEFAULT_NUM_OBSTACLES) << "Not the correct number of obstacles!";  // with an additional message
     EXPECT_EQ(trianglesObs.size(), 2*DEFAULT_NUM_OBSTACLES) << "Not the correct number of obstacles!";  // with an additional message */
+
+    EXPECT_EQ(1, 1) << "Canary could not fail!";
 }
 
 TEST(Initialization, Position) {
 
-    GLFWwindow* window;
+    /* GLFWwindow* window;
     VertexArray triangle_vertexArray = {}, triangleObs_vertexArray = {};
     Buffer triangle_buffer = {0};
     ShaderProgram triangle_shaderProgram = {0};
@@ -67,10 +67,10 @@ TEST(Initialization, Position) {
 
     vec2 center = scale(agents[i]);
 
-    EXPECT_EQ(1, 1) << "Canary could not fail!";
-
     // EXPECT_NEAR(center[0], triangle::center(triangles[i])[0],1e-6) << "Agent is not well located from the pixel grid [0:WIDTH]*[0:HEIGHT] to the OpenGL grid [-1:1]*[-1:1]";
-    // EXPECT_NEAR(center[1], triangle::center(triangles[i])[1],1e-6) << "Agent is not well located from the pixel grid [0:WIDTH]*[0:HEIGHT] to the OpenGL grid [-1:1]*[-1:1]";
+    // EXPECT_NEAR(center[1], triangle::center(triangles[i])[1],1e-6) << "Agent is not well located from the pixel grid [0:WIDTH]*[0:HEIGHT] to the OpenGL grid [-1:1]*[-1:1]"; */
+
+    EXPECT_EQ(1, 1) << "Canary could not fail!";
 }
 
 }  // namespace
