c47fbbb22b1bdccad60eade31d7ac972ee1a6aaf
Ugo <upelissier@gmail.com>
2022-10-17 11:11:59 +0200

    First GTest try

--- test/unit_gtest.cpp
+++ test/unit_gtest.cpp
@@ -20,43 +20,51 @@ TEST(FirstGTest, ExpectCanary) {
 
 TEST(Initialization, VectorSize) {
 
-    /* GLFWwindow* window;
-    VertexArray triangle_vertexArray = {}, triangleObs_vertexArray = {};
-    Buffer triangle_buffer = {0};
-    ShaderProgram triangle_shaderProgram = {0};
-    GLint mvp_location;
-    std::tie(window, triangle_vertexArray, triangleObs_vertexArray, triangle_buffer, triangle_shaderProgram, mvp_location) = initWindow();
+    std::vector<Obstacle> obstacles = initObstacles();
+    std::vector<Agent> agents = initialiaze_agents(obstacles);
 
-    // Agents initialization
-    std::vector<Agent> agents;
-    std::vector<Obstacle> obstacles;
-    std::vector<std::array<triangle::Vertex, 3>> triangles; // Array that will contain the birds, represented with triangles
+    std::vector<std::array<triangle::Vertex, 3>> triangles;
     std::vector<std::array<triangle::Vertex, 3>> trianglesObs;
-    std::tie(agents, obstacles,triangles, trianglesObs) = initAgentWindow();
+    std::vector<std::array<triangle::Vertex, 3>> obstacle;
+
+    for (Agent agent : agents) {
+        if ( agent.get_predator() )
+            triangles.push_back(triangle::newTriangle(scale(agent), PRED_COLOR, agent.get_angle(), 2 * BODY_SIZE));
+        else
+            triangles.push_back(triangle::newTriangle(scale(agent), BIRD_COLOR, agent.get_angle(), BODY_SIZE));
+    }
+    for (Obstacle obs : obstacles) {
+        obstacle = triangle::newObstacle(scale(obs), OBSTACLE_COLOR, obs.get_height()/HEIGHT, obs.get_width()/WIDTH);
+        trianglesObs.push_back(obstacle[0]);
+        trianglesObs.push_back(obstacle[1]);
+    }
 
     EXPECT_EQ(agents.size(), DEFAULT_NUM_AGENTS+DEFAULT_NUM_PREDATORS) << "Not the correct number of agents!";  // with an additional message
     EXPECT_EQ(triangles.size(), DEFAULT_NUM_AGENTS+DEFAULT_NUM_PREDATORS) << "Not the correct number of agents!";  // with an additional message
     EXPECT_EQ(obstacles.size(), DEFAULT_NUM_OBSTACLES) << "Not the correct number of obstacles!";  // with an additional message
-    EXPECT_EQ(trianglesObs.size(), 2*DEFAULT_NUM_OBSTACLES) << "Not the correct number of obstacles!";  // with an additional message */
-
-    EXPECT_EQ(1, 1) << "Canary could not fail!";
+    EXPECT_EQ(trianglesObs.size(), 2*DEFAULT_NUM_OBSTACLES) << "Not the correct number of obstacles!";  // with an additional message
 }
 
 TEST(Initialization, Position) {
 
-    /* GLFWwindow* window;
-    VertexArray triangle_vertexArray = {}, triangleObs_vertexArray = {};
-    Buffer triangle_buffer = {0};
-    ShaderProgram triangle_shaderProgram = {0};
-    GLint mvp_location;
-    std::tie(window, triangle_vertexArray, triangleObs_vertexArray, triangle_buffer, triangle_shaderProgram, mvp_location) = initWindow();
+    std::vector<Obstacle> obstacles = initObstacles();
+    std::vector<Agent> agents = initialiaze_agents(obstacles);
 
-    // Agents initialization
-    std::vector<Agent> agents;
-    std::vector<Obstacle> obstacles;
-    std::vector<std::array<triangle::Vertex, 3>> triangles; // Array that will contain the birds, represented with triangles
+    std::vector<std::array<triangle::Vertex, 3>> triangles;
     std::vector<std::array<triangle::Vertex, 3>> trianglesObs;
-    std::tie(agents, obstacles,triangles, trianglesObs) = initAgentWindow();
+    std::vector<std::array<triangle::Vertex, 3>> obstacle;
+
+    for (Agent agent : agents) {
+        if ( agent.get_predator() )
+            triangles.push_back(triangle::newTriangle(scale(agent), PRED_COLOR, agent.get_angle(), 2 * BODY_SIZE));
+        else
+            triangles.push_back(triangle::newTriangle(scale(agent), BIRD_COLOR, agent.get_angle(), BODY_SIZE));
+    }
+    for (Obstacle obs : obstacles) {
+        obstacle = triangle::newObstacle(scale(obs), OBSTACLE_COLOR, obs.get_height()/HEIGHT, obs.get_width()/WIDTH);
+        trianglesObs.push_back(obstacle[0]);
+        trianglesObs.push_back(obstacle[1]);
+    }
 
     int N = DEFAULT_NUM_AGENTS+DEFAULT_NUM_PREDATORS;
     std::uniform_int_distribution uni(0, N); // Uniform distribution on [0:1] => Random number between 0 and 1
@@ -67,10 +75,8 @@ TEST(Initialization, Position) {
 
     vec2 center = scale(agents[i]);
 
-    // EXPECT_NEAR(center[0], triangle::center(triangles[i])[0],1e-6) << "Agent is not well located from the pixel grid [0:WIDTH]*[0:HEIGHT] to the OpenGL grid [-1:1]*[-1:1]";
-    // EXPECT_NEAR(center[1], triangle::center(triangles[i])[1],1e-6) << "Agent is not well located from the pixel grid [0:WIDTH]*[0:HEIGHT] to the OpenGL grid [-1:1]*[-1:1]"; */
-
-    EXPECT_EQ(1, 1) << "Canary could not fail!";
-}
+    EXPECT_NEAR(center[0], triangle::center(triangles[i])[0],1e-6) << "Agent is not well located from the pixel grid [0:WIDTH]*[0:HEIGHT] to the OpenGL grid [-1:1]*[-1:1]";
+    EXPECT_NEAR(center[1], triangle::center(triangles[i])[1],1e-6) << "Agent is not well located from the pixel grid [0:WIDTH]*[0:HEIGHT] to the OpenGL grid [-1:1]*[-1:1]";
+    }
 
 }  // namespace
